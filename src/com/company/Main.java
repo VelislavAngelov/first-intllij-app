9b60b90e121ecceafecd88b756ff1f1938c289c2
VelislavAngelov <Krisy@DESKTOP-9R6M3OA>
2022-04-05 12:39:01 +0300

    firstCommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	   System.out.println("Hello Evryone");
+        String s = "Vely";
+        if(args.length>0){
+            s = args[0];
+        }
+        Main m = new Main();
+        m.run();
 
+
+
+    }
+
+    private void run() {
+        System.out.println("Hello World !");
     }
 }
